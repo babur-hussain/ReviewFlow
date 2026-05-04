ba9f195d178a1961b0385a05759f7fc85c3f8c80
baburhussain <baburhussain660@gmail.com>
2026-05-05 02:27:06 +0530

    chore: expose error details in error handler

--- backend/src/middleware/errorHandler.js
+++ backend/src/middleware/errorHandler.js
@@ -2,7 +2,8 @@ function errorHandler(err, req, res, next) {
   console.error(err);
   if (res.headersSent) return next(err);
   res.status(err.status || 500).json({
-    message: err.publicMessage || "Something went wrong",
+    message: err.publicMessage || err.message || "Something went wrong",
+    stack: err.stack,
   });
 }
 
